6ce4526b46ef2d06a41510dc0f98db4547fc3415
Jorge Velasco <jorge@typeform.com>
2018-06-08 14:39:29 +0200

    Serve static files from server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(`${process.cwd()}/out`))
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname+'./../out'})
